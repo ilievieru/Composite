0b273c07520c7986b7ac8d9eaf8584433f60739c
ilievieru <ilie_vieru_2010@yahoo.com>
2016-05-04 14:00:26 +0300

    Composite and  test2

---
+++ src/main/java/Exceptions/WrongPointsExceptions.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+/**
+ * Created by V3790149 on 5/4/2016.
+ */
+public class WrongPointsExceptions extends RuntimeException {
+    public WrongPointsExceptions(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/Exceptions/WrongSizeExceptions.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+/**
+ * Created by V3790149 on 5/4/2016.
+ */
+public class WrongSizeExceptions extends RuntimeException{
+    public WrongSizeExceptions(String message){
+        super(message);
+    }
+}
